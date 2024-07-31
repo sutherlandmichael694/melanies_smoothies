2d8456f9244e8fdef845aafb199aac89fc2d6b2c
sutherlandmichael694 <sutherlandmichael694@gmail.com>
2024-07-31 13:59:32 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,5 +47,5 @@ if time_to_insert:
 # new section to imprto fruityvice nutrition information
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
 
