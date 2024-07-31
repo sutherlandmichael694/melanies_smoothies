f1368e6bef2cadeab5fbf999c9d2ca9fb5320e71
sutherlandmichael694 <sutherlandmichael694@gmail.com>
2024-07-31 14:21:13 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,6 +33,7 @@ if ingredients_list:
         st.subheader (fruit_chosen + ' Nutrional Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width = True)
+        st.write("https://fruityvice.com/api/fruit/" + fruit_chosen)
 
 my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string + """','""" + name_on_order +  """')"""
