c11be3735ef967c6a938b4347c3abcb78aafa927
sutherlandmichael694 <sutherlandmichael694@gmail.com>
2024-07-31 13:36:35 +0100

    Update streamlit_app.py
    removed captial from 'snowflake' connection call

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 
-cnx = st.connection("Snowflake")
+cnx = st.connection("snowflake")
 session = cnx.session()
 
 # Write directly to the app
